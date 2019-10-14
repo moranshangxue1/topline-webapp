81fb87425b9ff950d05db45630e6c473919f53d1
love-cong <2404662491@qq.com>
2019-10-14 20:23:49 +0800

    扩展自定义校验规则

--- src/main.js
+++ src/main.js
@@ -21,6 +21,18 @@ for (let rule in rules) {
 // 注册为全局组件
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.component('ValidationObserver', ValidationObserver)
+
+// 扩展自定义校验规则
+// extend('规则名称', 配置对象)
+extend('phone', {
+  //  验证方法。value 是需要校验的数据，返回一个布尔值，表示验证成功与否
+  validate: function (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  // 错误提示消息
+  message: '请输入有效的手机号码'
+})
+
 Vue
   .use(NavBar)
   .use(Field)
