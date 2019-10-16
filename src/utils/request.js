e9a3031d2052b6a61a91c034690868a53cd1dc55
love-cong <2404662491@qq.com>
2019-10-16 16:49:05 +0800

    Token处理-使用请求拦截器统一添加token

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,9 @@
  */
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+// 在非组件模块中使用容器就直接加载即可
+// 这里获取到的 store 和在组件中的this.$store 是一个东西
+import store from '@/store'
 
 // axios.create 方法返回一个请求对象， 这个请求对象的功能和axios的功能是一样的
 // 也就是说我们复制了一个axios
@@ -18,8 +21,22 @@ request.defaults.transformResponse = [function (data) {
   return data ? jsonBig.parse(data) : {} // 换了一个转化方法 使得 计算更精确 保证id不失真
 }]
 /**
- * 请求拦截器 */
-
+ * 请求拦截器，请求到ad后台之前拦截 */
+request.interceptors.request.use(function (config) {
+  // 在发起请求做一些业务处理
+  // config是要发送请求的配置信息
+  const user = store.state.user
+  if (user) {
+    // Authorization 是后端要求的名字 不能瞎写
+    // 数据值 "Bearer空格token" 也是后端要求的数格式，不能瞎写
+    // 注意 、、空格不能少
+    config.headers['Authorization'] = `Bearer ${user.token}` // 统一注入token 到headers属性 因为所有要求接口要求的token格式是一样的
+  }
+  // 统一注入token 到headers属性 因为所有接口的要求
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
 /**
  * 响应拦截器 */
 // 导出这个请求对象，哪里需要发请求，哪里就加载使用
