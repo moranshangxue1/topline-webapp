ff3ab15181e1781a7db8429fe344d983d8a04a87
love-cong <2404662491@qq.com>
2019-10-20 15:57:03 +0800

    搜索-搜索结果-布局

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -1,13 +1,66 @@
 <template>
-  <div class="search-result">搜索结果</div>
+  <div class="serach-result">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      title="xxx 的搜索结果"
+      left-arrow
+      fixed
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+
+    <!-- 文章列表 -->
+    <van-list
+      class="article-list"
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
+    <!-- /文章列表 -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SarchResult'
+  name: 'SearchResult',
+  data () {
+    return {
+      list: [], // 数据列表
+      loading: false, // 上拉加载更多的loading
+      finished: false // 是否加载结束
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+   .serach-result {
+       .article-list {
+           margin-top: 46px;
+       }
+   }
 </style>
