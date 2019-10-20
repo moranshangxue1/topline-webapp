f24dd7f44c5db4825cc44cbf16b6883caa1a105c
love-cong <2404662491@qq.com>
2019-10-20 15:22:37 +0800

    搜索-传递搜索参数

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -6,10 +6,10 @@
       placeholder="请输入搜索关键词"
       show-action
       shape="round"
-      @search="onSearch"
+      @search="onSearch(searchText)"
       @input="onSearchInput"
     >
-      <div slot="action" @click="onSearch">搜索</div>
+      <div slot="action" @click="onSearch(searchText)">搜索</div>
     </van-search>
     <!-- /搜索框 -->
 
@@ -19,8 +19,9 @@
         icon="search"
         v-for="(item, index) in searchSuggestions"
         :key="index"
+        @click="onSeach(item)"
       >
-       <div v-html="item" slot="title"></div>
+       <div v-html="highlight(item)" slot="title"></div>
       </van-cell>
     </van-cell-group>
     <!-- /联想建议 -->
@@ -40,8 +41,8 @@ export default {
   },
 
   methods: {
-    onSearch () {
-      console.log('onSearch')
+    onSearch (str) {
+      this.$router.push('/search/ + str')
     },
 
     async onSearchInput () {
@@ -57,13 +58,19 @@ export default {
       const searchSuggestions = data.data.options
 
       // 根据一个字符串创建一个正则表达式对象
-      const reg = new RegExp(searchText, 'g')
+      // const reg = new RegExp(searchText, 'g')
 
-      searchSuggestions.forEach((item, index) => {
-        searchSuggestions[index] = item.replace(reg, '<span style="color: red">' + searchText + '</span>')
-      })
+      // searchSuggestions.forEach((item, index) => {
+      // searchSuggestions[index] = item.replace(reg, '<span style="color: red">' + searchText + '</span>')
+      // })
 
       this.searchSuggestions = searchSuggestions
+    },
+    highlight (str) {
+      // 'AA'
+      // 'a'、 'a1'、'aaa'
+      const reg = new RegExp(this.searchText, 'g')
+      return str.replace(reg, '<span style="color: red">' + this.searchText + '</span>')
     }
   }
 }
